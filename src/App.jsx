385f83230358ba96fb2e7ca7c4bac3ccaa74ee06
artimusmaximus <arthurmodymaniv@gmail.com>
2025-01-07 11:35:06 -0800

    testing fresh build

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   // npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch  // dev
   // npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch  // production build run from root folder, not src
 console.log("%c Made with Web ♥ by AMIV ivthe.art", "background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%); color:black; font-size: 24px;");
+console.info("Containerized 01 07 2025");
 
   return (
     <div className="App h-full w-full pb-24">
